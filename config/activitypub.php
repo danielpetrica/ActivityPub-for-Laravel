330d9b2e449a7bef5e5b329f27997353f958b6a5
Daniel Petrica <andrei@danielpetrica.com>
2026-06-21 16:39:41 +0900

    fix: publish config/activitypub.php so fediverse routes survive config:cache

---
+++ config/activitypub.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'domain' => env('ACTIVITYPUB_DOMAIN', env('APP_URL')),
+
+    'routes' => [
+        'enabled' => true,
+        'prefix' => '',
+        'middleware' => ['api'],
+    ],
+
+    'actor_model' => DanielPetrica\LaravelActivityPub\Models\Actor::class,
+
+    'http_signatures' => [
+        'enabled' => true,
+        'max_clock_skew' => 300,
+    ],
+
+    'federation' => [
+        'enabled' => env('ACTIVITYPUB_FEDERATION_ENABLED', false),
+        'max_delivery_attempts' => 3,
+        'delivery_timeout' => 10,
+        'user_agent' => 'danielpetrica/laravel-activitypub (+https://danielpetrica.com)',
+    ],
+
+    'fediverse' => [
+        'enabled' => env('ACTIVITYPUB_FEDIVERSE_ENABLED', true),
+        'prefix' => 'fediverse',
+        'middleware' => ['web', 'auth'],
+    ],
+];
