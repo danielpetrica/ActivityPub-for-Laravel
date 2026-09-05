e4940f3dbfeb8c59fdf22b899c982123a5120e9d
Daniel Petrica <andrei@danielpetrica.com>
2026-09-06 00:49:34 +0900

    feat: add Plausible analytics script to app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -67,6 +67,14 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    {{-- Plausible Analytics --}}
+    <!-- Privacy-friendly analytics by Plausible -->
+    <script async src="https://hello.danielpetrica.com/js/pa-wCrpja3diwz02iECjHLdq.js"></script>
+    <script>
+      window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+      plausible.init()
+    </script>
+
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap');
